91537d122bcfe68605fd2cacc8d71c9933bcfe05
Armyluck111 <armyluck111@gmail.com>
2023-11-27 14:24:33 +0200

    Dola

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { } from "@fortawesome/free-solid-svg-icons";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+  
+    </div>
+  );
+}
+export default App;
